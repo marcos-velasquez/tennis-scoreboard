94d3e54ff5d6493e040db21b25793aada05eeb88
Marcos Velasquez <marcos@socialgest.net>
2025-07-13 16:48:42 -0400

    refactor: improve service class with MAX_FAULTS constant and cleaner player switching logic

--- src/models/player.ts
+++ src/models/player.ts
@@ -1,11 +1,11 @@
 export class Player {
   constructor(public readonly name: string) {}
 
-  public static many(...names: string[]): Player[] {
-    return names.map((name) => new Player(name));
-  }
-
   public equals(other: Player): boolean {
     return this.name === other.name;
+  }
+
+  public static many(...names: string[]): Player[] {
+    return names.map((name) => new Player(name));
   }
 }
--- src/models/service.ts
+++ src/models/service.ts
@@ -2,35 +2,35 @@ import assert from 'assert';
 import { Player } from './player';
 
 export class Service {
-  private players: Player[];
-  private take: number;
+  public static readonly MAX_FAULTS = 2;
+  private serverIndex: number;
   private faultedCount: number = 0;
 
-  constructor(players: Player[]) {
-    this.take = Math.floor(Math.random() * players.length);
-    this.players = players;
+  constructor(private readonly players: Player[]) {
+    this.serverIndex = Math.floor(Math.random() * players.length);
   }
 
   public switchPlayer(): void {
-    this.take = (this.take + 1) % this.players.length;
+    this.serverIndex = (this.serverIndex + 1) % this.players.length;
     this.resetFault();
   }
 
   public registerFault(): void {
-    assert(this.faultedCount < 2, 'The second fault has already been registered');
+    assert(this.faultedCount < Service.MAX_FAULTS, 'The second fault has already been registered');
     this.faultedCount++;
   }
 
   public isSecondFault(): boolean {
-    return this.faultedCount === 2;
+    return this.faultedCount === Service.MAX_FAULTS;
   }
 
   public getCurrentPlayer(): Player {
-    return this.players[this.take];
+    return this.players[this.serverIndex];
   }
 
   public getRestPlayer(): Player {
-    return this.players.find((p) => p !== this.getCurrentPlayer())!;
+    const otherIndex = (this.serverIndex + 1) % this.players.length;
+    return this.players[otherIndex];
   }
 
   public hasFaulted(): boolean {
